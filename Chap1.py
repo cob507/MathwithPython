fd7521a5678c4b1d1b15343ae4f8294cd0cb0165
Student <student@Connor-OBriens-MacBook-Air.local>
2017-01-19 11:36:41 -0500

    January 19th 2017

---
+++ Chap1.py
@@ -0,0 +1,48 @@
+# Addition and subtraction
+
+print (1 + 2)
+print (1 + 3.5)
+print (-1 + 2.5)
+print (100 - 45)
+print (-1.1 + 5)
+
+# Multiplication
+
+print (3 * 2)
+print (3.5 * 1.5)
+
+# Division (with decimal place)
+
+print (3 / 2)
+print (4 / 2)
+
+# Division (Just integers)
+
+print (3 // 2)
+print (-3 // 2)
+
+# Division (Just the remainder)
+
+print (9 % 2)
+
+# ** Means Exponent
+
+print (2 ** 2)
+print (2 ** 10)
+print (1 ** 10)
+
+# We can also use the exponential symbol to calculate powers less than 1.
+# For example, the square root of a number n can be expressed as n^1/2
+# and the cube root as n^1/3:
+
+print (8 ** (1/3))
+
+# you can use parentheses to combine mathematical operations into
+# more complicated expressions. Python will evaluate the expression
+# following the standard PEMDAS rule for the order of
+# calculations—parentheses, exponents, multiplication, division, addition,
+# and subtraction. Consider the following two expressions—one without
+# parentheses and one with:
+
+print (5 + 5 * 5)
+print ((5 + 5) * 5)
